a765d98b5a94ef8b70e0ef5a7bb5000d48d908ec
BraveWorm <braveworm9@gmail.com>
2021-11-14 22:47:13 +0100

    bez ciasteczek

--- src/index.js
+++ src/index.js
@@ -13,6 +13,60 @@ app.use(express.urlencoded({extended: true}))
 
 app.get('/api/', (req, res) => res.send('OK!'))
 
+
+// User
+app.get('/api/users', authenticate, (req, res) => {
+    const users = [{ id:1, name: 'Adam'}]
+
+    res.send(users)
+})
+
+app.post('/api/auth/login', (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    // const validPassword = await bcrypt.compare(password, user[0].password)
+
+    const accessToken = jwt.sign({ id: 1}, process.env.TOKEN_SECRET, { expiresIn: 86400 })
+    const refreshToken = jwt.sign({ id: 1}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: 525600 })
+    // CSRF
+    res.send({ accessToken, refreshToken})
+})
+
+app.post('/api/auth/refresh', async (req, res) => {
+    const refreshToken = req.body.token
+
+    if (!refreshToken) {
+        return res.status(401)
+    }
+
+    // TODO: Check if refreshToken exist in DB
+
+    try {
+        await jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+    } catch (err) {
+        return res.sendStatus(403)
+    }
+
+    const accessToken = jwt.sign({ id: 1}, process.env.TOKEN_SECRET, { expiresIn: 86400 })
+
+    res.send({ accessToken })
+})
+
+function authenticate(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token === null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+
+        req.user = user
+        next()
+    })
+}
+
 app.listen(3001, () => {
     console.log('Server is up!')
 })
