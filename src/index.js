2a30b3c279c348950aaea8aa6d340370effcd51d
BraveWorm <braveworm9@gmail.com>
2021-11-15 18:51:36 +0100

    register login

--- src/index.js
+++ src/index.js
@@ -3,34 +3,55 @@ import jwt from 'jsonwebtoken'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import User from './models/User'
+import knex from './config/database'
 
 dotenv.config()
 
+const bcrypt = require('bcrypt')
 const app = express()
 
 app.use(express.json())
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/api/', (req, res) => res.send('OK!'))
 
 
 // User
 app.get('/api/users', async (req, res) => {
+    //app.get('/api/users', async (req, res) => {
     const users = await User.query()
 
     res.send(users)
 })
 
+app.post("/api/auth/registration", (request, response, next) => {
+    bcrypt.hash(request.body.password, 8)
+        .then(hashedPassword => {
+            return knex("users").insert({
+                //id: "", 
+                email: request.body.email,
+                password: hashedPassword
+            })
+                .returning(["id", "email"])
+                .then(users => {
+                    response.json(users[0])
+                })
+                .catch(error => next(error))
+        })
+})
+
+
+/*
 app.post('/api/auth/login', (req, res) => {
     const email = req.body.email
     const password = req.body.password
 
     // const validPassword = await bcrypt.compare(password, user[0].password)
 
-    const accessToken = jwt.sign({ id: 1}, process.env.TOKEN_SECRET, { expiresIn: 86400 })
+    const accessToken = jwt.sign({ id: 1}, users, { expiresIn: 86400 })
     const refreshToken = jwt.sign({ id: 1}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: 525600 })
 
     res.cookie('JWT', accessToken, {
@@ -39,6 +60,42 @@ app.post('/api/auth/login', (req, res) => {
     })
 
     res.send({ accessToken, refreshToken})
+})*/
+
+app.post('/api/auth/login', (request, response) => {
+    knex("users")
+        .where({ email: request.body.email })//!!!!!
+        .first()
+        .then(users => {
+            if (!users) {
+                response.status(401).json({
+                    error: "No users by that name"
+                })
+            } else {
+                return bcrypt
+                    .compare(request.body.password, users.password)//!!!!
+                    .then(isAuthenticated => {
+                        if (!isAuthenticated) {
+                            response.status(401).json({
+                                error: "Unauthorized Access!"
+                            })
+                        } else {
+                            /*return jwt.sign(users, process.env.TOKEN_SECRET, (error, token) => {
+                               response.status(200).json({token})
+                            })*/
+                            const accessToken = jwt.sign({ id: 1 }, users, { expiresIn: 86400 })
+                            const refreshToken = jwt.sign({ id: 1 }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: 525600 })
+
+                            res.cookie('JWT', accessToken, {
+                                maxAge: 8640000,
+                                httpOnly: true,
+                            })
+
+                            res.send({ accessToken, refreshToken })
+                        }
+                    })
+            }
+        })
 })
 
 app.post('/api/auth/refresh', async (req, res) => {
@@ -56,12 +113,12 @@ app.post('/api/auth/refresh', async (req, res) => {
         return res.sendStatus(403)
     }
 
-    const accessToken = jwt.sign({ id: 1}, process.env.TOKEN_SECRET, { expiresIn: 86400 })
+    const accessToken = jwt.sign({ id: 1 }, process.env.TOKEN_SECRET, { expiresIn: 86400 })
 
     res.send({ accessToken })
 })
 
-function authenticate(req, res, next){
+function authenticate(req, res, next) {
     // const authHeader = req.headers['authorization']
     // const token = authHeader && authHeader.split(' ')[1]
 
