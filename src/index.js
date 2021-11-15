f83a0ad61c76dbd218273480d38a17a1ecdefc2c
BraveWorm <braveworm9@gmail.com>
2021-11-15 01:15:06 +0100

    JWT i User

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import User from './models/User'
 
 dotenv.config()
 
@@ -9,14 +11,15 @@ const app = express()
 app.use(express.json())
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/api/', (req, res) => res.send('OK!'))
 
 
 // User
-app.get('/api/users', authenticate, (req, res) => {
-    const users = [{ id:1, name: 'Adam'}]
+app.get('/api/users', async (req, res) => {
+    const users = await User.query()
 
     res.send(users)
 })
@@ -29,7 +32,12 @@ app.post('/api/auth/login', (req, res) => {
 
     const accessToken = jwt.sign({ id: 1}, process.env.TOKEN_SECRET, { expiresIn: 86400 })
     const refreshToken = jwt.sign({ id: 1}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: 525600 })
-    // CSRF
+
+    res.cookie('JWT', accessToken, {
+        maxAge:8640000,
+        httpOnly: true,
+    })
+
     res.send({ accessToken, refreshToken})
 })
 
@@ -54,8 +62,8 @@ app.post('/api/auth/refresh', async (req, res) => {
 })
 
 function authenticate(req, res, next){
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
+    // const authHeader = req.headers['authorization']
+    // const token = authHeader && authHeader.split(' ')[1]
 
     if (token === null) return res.sendStatus(401)
 
